80dad99ae7b8b21484cefcfd873d72b4979f9086
Swan Blanc <swan.blanc@infomaniak.com>
2021-10-14 11:06:56 +0200

    Add motion_detection.py

---
+++ home_pi/motion_detection.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import datetime
+import time
+
+import cv2
+import imutils
+import numpy as np
+from imutils.video import VideoStream
+
+from .conf import Conf
+from .keyclipwriter import KeyClipWriter
+
+
+class MotionDetection:
+    GREEN_COLOR = (0, 255, 0)
+
+    def __init__(self, conf_path: str):
+        """
+
+        Args:
+            conf_path:
+        """
+        # load the configuration file
+        self.conf = Conf(conf_path)
+        # initialize key clip writer and the consecutive number of
+        # frames that have *not* contained any action
+        self.kcw = KeyClipWriter(bufSize=self.conf["buffer_size"])
+        self.consec_frames = 0
+
+        # initialize the MOG foreground background subtractor
+        self.fgbg = cv2.bgsegm.createBackgroundSubtractorMOG(
+            history=self.conf["mog_history"],
+            nmixtures=self.conf["mog_nmixtures"],
+            backgroundRatio=self.conf["mog_bg_ratio"],
+            noiseSigma=self.conf["mog_noise_sigma"])
+
+        # create erosion and dilation kernels
+        self.eKernel = np.ones((7, 7), np.uint8)
+        self.dKernel = np.ones((3, 3), np.uint8)
+        self.update_consec_frames = True
+
+        self.video_stream = VideoStream(usePiCamera=self.conf["picamera"]).start()
+        time.sleep(2.0)
+
+    def get_frame(self):
+        """
+
+        Returns:
+
+        """
+        # grab the current frame, resize it, and initialize a
+        # boolean used to indicate if the consecutive frames
+        # counter should be updated
+        frame = self.video_stream.read()
+        frame = imutils.resize(frame, width=600)
+
+        # flag to update the counter consecutive frames with no motion
+        self.update_consec_frames = True
+
+        # apply forground background subtraction with the
+        # GMG algorithm
+        mask = self.fgbg.apply(frame)
+
+        # erode the mask to eliminate noise and then
+        # dilate the mask to fill in holes
+        mask = cv2.erode(mask, self.eKernel, iterations=3)
+        mask = cv2.dilate(mask, self.dKernel, iterations=2)
+
+        # find contours
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+
+        # only proceed if at least one contour was found
+        if len(cnts) > 0:
+            # find the largest contour in the mask, then use it
+            # to compute the minimum enclosing circle
+            c = max(cnts, key=cv2.contourArea)
+            (x, y, w, h) = cv2.boundingRect(c)
+            self.update_consec_frames = (w * h) <= self.conf["min_area"]
+            if self.update_consec_frames:
+                self.consec_frames = 0
+                return {
+                    "frame": frame,
+                    "x": x,
+                    "y": y,
+                    "width": w,
+                    "height": h
+                }
+
+    def save_frame(self, frame):
+        """
+
+        Args:
+            frame:
+
+        Returns:
+
+        """
+        # otherwise, no action has taken place in this frame, so
+        # increment the number of consecutive frames that contain
+        # no action
+        if self.update_consec_frames:
+            self.consec_frames += 1
+
+        # update the key frame clip buffer
+        self.kcw.update(frame)
+
+        # if we are recording and reached a threshold on consecutive
+        # number of frames with no action, stop recording the clip
+        if self.kcw.recording and self.consec_frames == self.conf["buffer_size"]:
+            self.kcw.finish()
+
+    @staticmethod
+    def draw_rectangle(frame, x, y, w, h, color):
+        """
+
+        Args:
+            frame:
+            x:
+            y:
+            w:
+            h:
+            color:
+
+        Returns:
+
+        """
+        cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+
+    def is_recording(self):
+        """
+
+        Returns:
+
+        """
+        return self.kcw.recording
+
+    def start_recording(self):
+        """
+
+        Returns:
+
+        """
+        # if we are not already recording, start recording
+        if not self.is_recording():
+            timestamp = datetime.datetime.now()
+            path = "{}/{}.avi".format(self.conf["output_videos_path"], timestamp.strftime("%Y%m%d-%H%M%S"))
+            self.kcw.start(path, cv2.VideoWriter_fourcc(*self.conf["codec"]), self.conf["output_fps"])
+
+    def __del__(self):
+        """
+
+        Returns:
+
+        """
+        # show a message to the user
+        print("[INFO] cleaning up...")
+
+        # if we are in the middle of recording a clip, wrap it up
+        if self.kcw.recording:
+            self.kcw.finish()
+
+        # otherwise, release the video file pointer
+        else:
+            self.video_stream.release()
--- tp03_camera.py
+++ tp03_camera.py
@@ -1,32 +1,32 @@
 import time
 
-from gpiozero import PWMLED, LED
+from gpiozero import PWMLED
 
 from home_pi.dht import DHT11
 
 
-def step(temperature, humidity, led, fan):
+def step(temperature, humidity, led):
     """
     Args:
         temperature: temperature get from the sensor in "c"
         humidity: humidity get from the sensor in "%"
         led: LED to control
-        fan: Fan to control
     """
     # Print information
     print("Temperature:", temperature)
     print("Humidity:", humidity)
+    led2 = PWMLED()
     # Turn on the LED
     led.value = 1.0
-    fan.on()
 
-    # TODO a toi de jouer ici !
+
+# --------------------------------
+# Ne pas modifier
 
 
 if __name__ == "__main__":
     instance = DHT11(pin=4)
     led = PWMLED(17)
-    fan = LED(27)
 
     # Wait 3s for init
     print("Wait 3s...")
@@ -36,7 +36,7 @@ if __name__ == "__main__":
         while True:
             result = instance.read()
             if result.is_valid():
-                step(result.temperature, result.humidity, led, fan)
+                step(result.temperature, result.humidity, led)
             time.sleep(3)
 
     except KeyboardInterrupt:
